1f7e3d548633a12de0aa5d228e1f72d7aed7334f
ashh910 <ashanddust910@gmail.com>
2026-07-08 16:11:35 +0500

    добавил short-term память агенту с помощью langgraph

--- agent_invoking.py
+++ agent_invoking.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
 from rag import search_documents
 from custom_llm import my_chatbot
 from langchain.agents.structured_output import ToolStrategy
@@ -12,15 +13,24 @@ agent = create_agent(
     model=my_chatbot,
     tools=[search_documents],
     response_format=Answer,
+    checkpointer = InMemorySaver(),
 )
 
+thread_config = { 
+    "configurable":
+    {
+        "thread_id": "1",
+    }
+}
+
 def agent_responses(question):
+    
     result = agent.invoke({"messages": [
             {
                 "role": "user", 
                 "content": question
             }
-        ]})
+        ]}, thread_config,)
 
     if "structured_response" in result:
         #return the structured response from the model
